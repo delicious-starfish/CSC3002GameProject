1643a3ffaa0afa57856bf767a607b664fe4249a0
3355178395 <115929432+3355178395@users.noreply.github.com>
2024-10-16 22:28:41 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,6 +9,17 @@
 #define LOGIC_FPS 10
 #define RENDER_FPS 10
 
+int currentTime;
+int currentRenderTime;
+int totalScore;
+int BeltAMOUNT;
+int CutterAMOUNT;
+int AveragerAMOUNT;
+int PorterAMOUNT;
+int ComposerAMOUNT;
+int RotatorAMOUNT;
+int MinerAMOUNT;
+
 long long getTime() {
     long long millisecSince1970 = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch())
         .count();
