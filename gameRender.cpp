d0e71ab1b313e3973bd9e5f56abb1659d9b72251
3355178395 <115929432+3355178395@users.noreply.github.com>
2024-10-20 22:47:17 +0800

    add function of alpha image

--- gameRender.cpp
+++ gameRender.cpp
@@ -15,6 +15,33 @@ void loadImgRes() {
 	loadimage(&rota, _T("tex\\rota.png"));
 }
 
+void putAlphaImage(int x, int y, IMAGE* srcimg)
+{
+    DWORD* dst = GetImageBuffer(NULL);
+    DWORD* src = GetImageBuffer(srcimg);
+    int src_width = srcimg->getwidth();
+    int src_height = srcimg->getheight();
+    int dst_width = getwidth();
+    int dst_height = getheight();
+    int iwidth = (x + src_width > dst_width) ? dst_width - x : src_width;
+    int iheight = (y + src_height > dst_height) ? dst_height - y : src_height;
+    if (x < 0) { src += -x;	iwidth -= -x;	x = 0; }
+    if (y < 0) { src += src_width * -y;	iheight -= -y;	y = 0; }
+    dst += dst_width * y + x;
+    for (int iy = 0; iy < iheight; ++iy)
+    {
+        for (int i = 0; i < iwidth; ++i)
+        {
+            int sa = ((src[i] & 0xff000000) >> 24);
+            if (sa != 0)
+                if (sa == 255)
+                    dst[i] = src[i];
+                else
+                    dst[i] = ((((src[i] & 0xff0000) >> 16) + ((dst[i] & 0xff0000) >> 16) * (255 - sa) / 256) << 16) | ((((src[i] & 0xff00) >> 8) + ((dst[i] & 0xff00) >> 8) * (255 - sa) / 256) << 8) | ((src[i] & 0xff) + (dst[i] & 0xff) * (255 - sa) / 256);
+        }
+        dst += dst_width; src += src_width;
+    }
+}
 
 void renderTick(World& world) {
 	cleardevice();
@@ -23,11 +50,11 @@ void renderTick(World& world) {
 			switch (world.mapp[i][j].type)
 			{
 			case(GROUNDID):
-				putimage(j* 32, i* 32, &grnd); break;
+				putAlphaImage(j* 32, i* 32, &grnd); break;
 			case(BELTID):
-				putimage(j * 32, i * 32, &belt); break;
+				putAlphaImage(j * 32, i * 32, &belt); break;
 			case(ROTATORID):
-				putimage(j * 32, i * 32, &rota); break;
+				putAlphaImage(j * 32, i * 32, &rota); break;
 
 			}
 		}
